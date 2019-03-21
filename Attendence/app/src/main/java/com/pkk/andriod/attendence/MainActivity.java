b5fe98b536cdbdd49172af64a224288f27a7199a
crazyassasin <kocherprateek2001@gmail.com>
2019-03-21 19:05:51 +0530

    Second commit

--- Attendence/app/src/main/java/com/pkk/andriod/attendence/MainActivity.java
+++ Attendence/app/src/main/java/com/pkk/andriod/attendence/MainActivity.java
@@ -11,12 +11,14 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
 import java.net.Socket;
 
 
@@ -30,10 +32,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonSend = (Button) findViewById(R.id.btn_send);
+        Button buttonSend =findViewById(R.id.btn_send);
 
-        mEditTextSendMessage = (EditText) findViewById(R.id.edt_send_message);
-        mTextViewReplyFromServer = (TextView) findViewById(R.id.tv_reply_from_server);
+        mEditTextSendMessage = findViewById(R.id.edt_send_message);
+        mTextViewReplyFromServer = findViewById(R.id.tv_reply_from_server);
 
         buttonSend.setOnClickListener(this);
     }
@@ -58,24 +60,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try {
                     //Replace below IP with the IP of that device in which server socket open.
                     //If you change port then change the port number in the server side code also.
-                    Socket s = new Socket("25.46.195.129", 9002);
+                    Toast.makeText(getApplicationContext(),"Device Pairing starting",Toast.LENGTH_SHORT);
+                    Socket s = new Socket(Inet4Address.getLocalHost().getHostAddress(), 9002);
+                    Toast.makeText(getApplicationContext(),"Socket is success",Toast.LENGTH_SHORT).show();
 
                     OutputStream out = s.getOutputStream();
+                    Toast.makeText(getApplicationContext(),"OutputStream is success",Toast.LENGTH_SHORT).show();
 
                     PrintWriter output = new PrintWriter(out);
+                    Toast.makeText(getApplicationContext(),"PrintWriter is success",Toast.LENGTH_SHORT).show();
 
                     output.println(msg);
                     output.flush();
-                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                    final String st = input.readLine();
+
+                    Toast.makeText(getApplicationContext(),"Message is send",Toast.LENGTH_SHORT).show();
+                    //BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    //final String st = input.readLine();
 
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
 
                             String s = mTextViewReplyFromServer.getText().toString();
-                            if (st.trim().length() != 0)
-                                mTextViewReplyFromServer.setText(s + "\n From Server : " + st);
+                            //if (st.trim().length() != 0)
+                                mTextViewReplyFromServer.setText(s + "\n From Server : "/* + st*/);
                         }
                     });
 
@@ -90,61 +98,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         thread.start();
     }
-
-    /*WifiP2pManager manager;
-    WifiP2pManager.Channel channel;
-    BroadcastReceiver receiver;
-    IntentFilter intentFilter;
-    TextView peertext;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(this, getMainLooper(), null);
-        receiver = new WiFiDirectBroadcastReceiver(manager, mChannel, this);
-
-        intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pMan ager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-
-                if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-
-                    // request available peers from the wifi p2p manager. This is an
-                    // asynchronous call and the calling activity is notified with a
-                    // callback on PeerListListener.onPeersAvailable()
-                    if (manager != null) {
-                        manager.requestPeers(channel, myPeerListListener);
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(int reasonCode) {
-                peertext.setText("Error :"+String.valueOf(reasonCode));
-            }
-        });
-    }
-
-    // register the broadcast receiver with the intent values to be matched
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(mReceiver, intentFilter);
-    }
-
-    // unregister the broadcast receiver
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(mReceiver);
-    }*/
 }
--- Attendence/app/src/main/java/com/pkk/andriod/attendence/WiFiDirectBroadcastReceiver.java
+++ Attendence/app/src/main/java/com/pkk/andriod/attendence/WiFiDirectBroadcastReceiver.java
@@ -38,4 +38,61 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             // Respond to this device's wifi state changing
         }
     }
+
+    /*WifiP2pManager manager;
+    WifiP2pManager.Channel channel;
+    BroadcastReceiver receiver;
+    IntentFilter intentFilter;
+    TextView peertext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+        receiver = new WiFiDirectBroadcastReceiver(manager, mChannel, this);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pMan ager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+                if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+                    // request available peers from the wifi p2p manager. This is an
+                    // asynchronous call and the calling activity is notified with a
+                    // callback on PeerListListener.onPeersAvailable()
+                    if (manager != null) {
+                        manager.requestPeers(channel, myPeerListListener);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                peertext.setText("Error :"+String.valueOf(reasonCode));
+            }
+        });
+    }
+
+    // register the broadcast receiver with the intent values to be matched
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, intentFilter);
+    }
+
+    // unregister the broadcast receiver
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }*/
 }
